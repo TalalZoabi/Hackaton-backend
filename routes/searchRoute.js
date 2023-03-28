e0d5d767cbeab7eeb3503aa7e58020167582e715
SuperSpawn <thearktitan@gmail.com>
2023-03-28 23:39:20 +0300

    expanded

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -5,128 +5,80 @@ const router = express.Router();
 const request = require("request");
 const cheerio = require("cheerio");
 
-//const url = "https://www.reddit.com/r/Minecraft/";
-// const selector = "div[data-scroller-first] + div";
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
-// router.get("/search", (req, res, next) => {
-//   const url = req.body.search; // Replace with the URL of the page you want to scrape
+const fetchPosts = async (url) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.reddit.com/r/" + url);
 
-//   request(url, (error, response, body) => {
-//     if (!error && response.statusCode === 200) {
-//       const $ = cheerio.load(body);
-//       const aLinks = $("h3")
-//         .map((i, element) => {
-//           console.log($(element).parent().parent().attr("href"));
-//           return (
-//             "https://www.reddit.com" + $(element).parent().parent().attr("href")
-//           );
-//         })
-//         .get();
-//       const h3Texts = $("h3")
-//         .map((i, element) => $(element).text())
-//         .get();
-//       const imageLinks = $("img")
-//         .map((i, element) => $(element).attr("src"))
-//         .get();
-//       console.log(h3Texts);
-//       console.log(aLinks);
-//       req.posts = h3Texts;
-//       //res.status(200).json(h3Texts);
-//       //next();
-//     } else {
-//       console.log(error);
-//     }
-//   });
-// });
+  let currentHeight = 0;
+  while (true) {
+    await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
+    await page.waitForTimeout(1000);
+    const newHeight = await page.evaluate("document.body.scrollHeight");
+    if (newHeight === currentHeight) {
+      break;
+    }
+    currentHeight = newHeight;
+  }
 
-// router.use(filterPosts);
+  const html = await page.content();
+  const $ = cheerio.load(html);
 
-// const subredditName = "minecraft";
+  const data = $("div.Post")
+    .map((i, el) => {
+      const title = $(el).find("h3").text().trim();
+      const link = $(el).find("a[data-click-id=body]").attr("href");
+      const image = $(el).find("img[class*='ImageBox-image']").attr("src");
 
-// const fetchPosts = async (url) => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto(url);
+      return { title, link, image };
+    })
+    .get();
 
-//   let currentHeight = 0;
-//   while (true) {
-//     await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
-//     await page.waitForTimeout(1000);
-//     const newHeight = await page.evaluate("document.body.scrollHeight");
-//     if (newHeight === currentHeight) {
-//       break;
-//     }
-//     currentHeight = newHeight;
-//   }
+  console.log(data);
 
-//   const html = await page.content();
-//   const $ = cheerio.load(html);
-
-//   const data = $("div.Post")
-//     .map((i, el) => {
-//       const title = $(el).find("h3").text().trim();
-//       const link = $(el).find("a[data-click-id=body]").attr("href");
-//       const image = $(el).find("img[class*='ImageBox-image']").attr("src");
-
-//       return {
-//         title,
-//         postLink: "https://www.reddit.com" + link,
-//         imageLink: "https://www.reddit.com" + image,
-//       };
-//     })
-//     .get();
-
-//   console.log(data);
-
-//   await browser.close();
-// };
-
-// router.get("/search", async (req, res, next) => {
-//   const url = req.body.search; // Replace with the URL of the page you want to scrape
-
-//   try {
-//     const data = await fetchPosts(url);
-//     req.posts = data;
-//     next();
-//   } catch (error) {
-//     res.status(500).json([]);
-//   }
-// });
-
-// router.use(filterPosts);
-
-const subredditName = "minecraft";
+  await browser.close();
+};
 
 router.post("/search", async (req, res, next) => {
   const url = "https://www.reddit.com/r/" + req.body.search;
-  console.log("url", url);
-  axios
-    .get(url, {
-      timeout: 30000, // Timeout value in milliseconds
-    })
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      console.log("first");
-      // Extract the data for each post
-
-      const data = $("div.Post")
-        .map((i, el) => {
-          const title = $(el).find("h3").text().trim();
-          const link = $(el).find("a[data-click-id=body]").attr("href");
-          const image = $(el).find("img[class*='ImageBox-image']").attr("src");
-
-          return { title, link, image };
-        })
-        .get();
-
-      // Do something with the data array here
-      req.posts = data;
-      next();
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  const data = await fetchPosts(url);
+  req.posts = data;
+  next();
 });
+
+// router.post("/search", async (req, res, next) => {
+//   const url = "https://www.reddit.com/r/" + req.body.search;
+//   console.log("url", url);
+//   axios
+//     .get(url, {
+//       timeout: 30000, // Timeout value in milliseconds
+//     })
+//     .then((response) => {
+//       const $ = cheerio.load(response.data);
+//       console.log("first");
+//       // Extract the data for each post
+
+//       const data = $("div.Post")
+//         .map((i, el) => {
+//           const title = $(el).find("h3").text().trim();
+//           const link = $(el).find("a[data-click-id=body]").attr("href");
+//           const image = $(el).find("img[class*='ImageBox-image']").attr("src");
+
+//           return { title, link, image };
+//         })
+//         .get();
+
+//       // Do something with the data array here
+//       req.posts = data;
+//       next();
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// });
 
 router.use(filterPosts);
 
